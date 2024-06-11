5e9a2a9fc1e8948b363968644ea793c3a67c9890
Zun79 <51515848+dbui6802@users.noreply.github.com>
2024-06-11 14:47:36 +0700

    Add files via upload

--- TutorLink/TutorLinkAPI/BusinessLogics/IServices/IAppointmentFeedback.cs
+++ TutorLink/TutorLinkAPI/BusinessLogics/IServices/IAppointmentFeedback.cs
@@ -2,5 +2,11 @@
 {
     public interface IAppointmentFeedback
     {
+        void BookAppointmentDate(int AppointmentId, DateTime AppointmentTime);
+        void NotifyTutorAboutAppointment(Guid? TutorId, DateTime AppointmentTime, string Address);
+        void HandleTutorConfirmation(Guid? TutorId, bool canGoToAppointment);
+        void SendFeedbackFormToParent(int AppointmentId, Guid? AccountId);
+        void UpdatePostStatus(Guid? PostId, AppStatuses Status);
+        void SendReviewReminderToParent(int AppointmentId, DateTime AppointmentTime);
     }
 }
