db67f7127db98509aa6d96c1c8512a0834908745
Zun79 <51515848+dbui6802@users.noreply.github.com>
2024-06-11 14:50:58 +0700

    Add files via upload

--- TutorLink/TutorLinkAPI/Controllers/AppointmentFeedbackController.cs
+++ TutorLink/TutorLinkAPI/Controllers/AppointmentFeedbackController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TutorLinkAPI.BusinessLogics.IServices;
 
 namespace TutorLinkAPI.Controllers
 {
@@ -6,10 +7,52 @@ namespace TutorLinkAPI.Controllers
     [ApiController]
     public class AppointmentFeedbackController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly IAppointmentFeedback _appointmentFeedbackService;
+        public AppointmentFeedbackController(IAppointmentFeedback appointmentFeedbackService)
         {
-            return View();
+            _appointmentFeedbackService = appointmentFeedbackService;
+        }
+
+        [HttpPost("book-appointment")]
+        public IActionResult BookAppointment(int AppointmentId, DateTime AppointmentTime)
+        {
+            _appointmentFeedbackService.BookAppointmentDate(AppointmentId, AppointmentTime);
+            return Ok("Appointment booked successfully.");
+        }
+
+        [HttpPost("notify-tutor")]
+        public IActionResult NotifyTutor(Guid? TutorId, DateTime AppointmentTime, string Address)
+        {
+            _appointmentFeedbackService.NotifyTutorAboutAppointment(TutorId, AppointmentTime, Address);
+            return Ok("Tutor notified about the appointment.");
+        }
+
+        [HttpPost("handle-tutor-confirmation")]
+        public IActionResult HandleTutorConfirmation(Guid? TutorId, bool canGoToAppointment)
+        {
+            _appointmentFeedbackService.HandleTutorConfirmation(TutorId, canGoToAppointment);
+            return Ok("Tutor confirmation handled successfully.");
+        }
+
+        [HttpGet("send-feedback-form")]
+        public IActionResult SendFeedbackForm(Guid AccountId, int AppointmentId)
+        {
+            _appointmentFeedbackService.SendFeedbackFormToParent(AppointmentId, AccountId);
+            return Ok("Feedback form sent to parent.");
+        }
+
+        [HttpPost("update-post-status")]
+        public IActionResult UpdatePostStatus(Guid? PostId, AppStatuses Status)
+        {
+            _appointmentFeedbackService.UpdatePostStatus(PostId, (AppStatuses)Status);
+            return Ok("Post status updated successfully.");
+        }
+
+        [HttpPost("send-review-reminder")]
+        public IActionResult SendReviewReminder(int AppointmentId, DateTime AppointmentTime)
+        {
+            _appointmentFeedbackService.SendReviewReminderToParent(AppointmentId, AppointmentTime);
+            return Ok("Review reminder sent to parent.");
         }
     }
 }
