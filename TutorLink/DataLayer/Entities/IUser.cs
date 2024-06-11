6068652925f7f45c6b15c7a90c85077ae105f55b
tintruong <tintruong194@gmail.com>
2024-06-11 13:28:24 +0700

    Add Login-Logout methods, JWT

---
+++ TutorLink/DataLayer/Entities/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public interface IUser
+    {
+        Guid UserId { get;}
+        string Username { get;}
+        string Email { get;}
+        int RoleId { get;}
+    }
+}
---
+++ TutorLink/TutorLinkAPI/BusinessLogics/IServices/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using DataLayer.Entities;
+using TutorLinkAPI.ViewModel;
+
+namespace TutorLinkAPI.BusinessLogics.IServices
+{
+    public interface IAuthService
+    {
+        AccessTokenViewModel GenerateToken(IUser user);
+        AccessTokenViewModel Login(string username, string password);
+    }
+}
