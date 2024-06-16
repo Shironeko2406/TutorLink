13a96a8a7a115e76a7bf98bbc67b35cb221f27e1
Tôn Thất Trung Hiếu <trunghieutonthat@gmail.com>
2024-06-16 15:01:44 +0700

    update account

--- TutorLink/TutorLinkAPI/BusinessLogics/IServices/IAccountService.cs
+++ TutorLink/TutorLinkAPI/BusinessLogics/IServices/IAccountService.cs
@@ -6,10 +6,10 @@ namespace TutorLinkAPI.BusinessLogics.IServices
 {
     public interface IAccountService
     {
-        void AddNewAccount(string username, string password, string fullname, string email, string phone, string address, UserGenders gender);
+        Account AddNewAccount(string username, string password, string fullname, string email, string phone, string address, UserGenders gender);
         IEnumerable<Account> GetAllAccounts();
         Account GetAccountById(Guid accountId);
-        void UpdateAccount(Guid accountId, string fullname, string email, string phone, string address, UserGenders gender);
+        void UpdateAccount(Guid id, string username, string password, string fullname, string email, string phone, string address, UserGenders gender);
         void DeleteAccount(Guid accountId);
         Account GetAccountEntityByUsername(string username);
         Account GetAccountEntityByUserId(Guid userId);
